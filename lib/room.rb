36b4991cc8a995e7daf3da951ab6d2e1d6bcb05c
Steffnay <steffannbrown@gmail.com>
2018-03-05 15:37:14 -0800

    add_new method added

--- lib/room.rb
+++ lib/room.rb
@@ -1,6 +1,11 @@
+require_relative '../lib/reservation.rb'
+require 'awesome_print'
+require 'pry'
+
 module Hotel
   class Room
     attr_reader :room_number, :price, :reservations
+    attr_accessor :reservation_info
 
     @@array = []
 
@@ -13,5 +18,16 @@ module Hotel
 
       return @room_number
     end
+
+    def add_new(room, start_date, end_date)
+      newby = Hotel::Reservation.new(room, start_date, end_date)
+
+      reservation_info = @@array.select {|num| num[:room] == room}
+      reservation_info[0][:reservations] << newby
+
+    end
   end
 end
+
+something = Hotel::Room.new(600)
+ap something.add_new(600, 2020-02-24, 2020-02-28)
